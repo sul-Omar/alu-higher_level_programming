502ec76794d09e71aa9c21554575adb48db963fe
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-15 14:52:41 -0700

    task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    largest = my_list[0]
+    for num in my_list:
+        if num > largest:
+            largest = num
+    return largest
