e651c4f446ee3b8d784858e28ad82b2ae6c5b751
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-15 14:48:45 -0700

    task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum = [0, 0]
+    for x in range(2):
+        if x < len(tuple_a):
+            sum[x] += tuple_a[x]
+        if x < len(tuple_b):
+            sum[x] += tuple_b[x]
+    return tuple(sum)
