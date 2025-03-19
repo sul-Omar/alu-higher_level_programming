5d4935e8243bb44c65cbb1fa6d5be2f249cab8af
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-19 08:04:19 -0700

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Private instance attribute
+"""
+
+
+class Square:
+    """
+    Defines a square
+    """
+    def __init__(self, size):
+        """Initialises the data"""
+        self.__size = size
