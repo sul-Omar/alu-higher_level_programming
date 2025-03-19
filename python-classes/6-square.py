3fdc62ccb5c245c35251d3d61edbcfbb19111a06
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-19 08:16:10 -0700

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this define class square with public attribute
+this is define square class with public instance attribute
 """
 
 
@@ -8,11 +8,12 @@ class Square:
     """
     Defines a square
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Initialises the data
         """
         self.size = size
+        self.position = position
 
     def area(self):
         """
@@ -32,11 +33,11 @@ class Square:
         """
         Setter method
         """
+        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
 
     def my_print(self):
         """
@@ -45,7 +46,27 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(' ', end='')
                 for j in range(self.__size):
                     print('#', end='')
                 print()
+
+    @property
+    def position(self):
+        """
+        Getter method
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter method"""
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if any(type(i) != int for i in value) or any(j < 0 for j in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
