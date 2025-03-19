ad4329c1b56f363c00bf2b0a9364b19f3be293f3
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-19 08:36:11 -0700

    task6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 8-rectangle
+"""Module 6-rectangle
 Defines a Rectangle class.
 """
 
@@ -13,7 +13,6 @@ class Rectangle:
     """
 
     number_of_instances = 0
-    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
@@ -33,7 +32,7 @@ class Rectangle:
         rec_str = ''
         for i in range(self.__height):
             for j in range(self.__width):
-                rec_str += str(self.print_symbol)
+                rec_str += '#'
             rec_str += '\n'
         return rec_str[:-1]
 
@@ -97,22 +96,3 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Finds the biggest Rectangle based on the area
-        Args:
-            rect_1: Rectangle instance
-            rect_2: Rectangle instance different from rect_1
-        Returns:
-            The instance with the biggest area,
-            or rect_1 if both rectangles have the same area
-        """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
-            return rect_1
-        if rect_1.area() < rect_2.area():
-            return rect_2
