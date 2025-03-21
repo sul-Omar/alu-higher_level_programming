e616fa4f75d988cf351b036908e4ee7f55298d3c
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-21 14:29:19 -0700

    task11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
-"""Square class Module"""
-Rectangle = __import__("9-rectangle").Rectangle
+
+"""
+This module defines the Square class that Rectangle.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    """A class representing a from Rectangle."""
+
     def __init__(self, size):
-        """Method for initializing a square"""
+        """
+        Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less to 0.
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Method that returns area of a square"""
-        return self.__size ** 2
+        """
+        Returns the area of the square (size * size).
+        """
+        return self.__size * self.__size
 
     def __str__(self):
-        """Method that returns a string"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """
+        Returns the string description of the square.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
+
+    def __repr__(self):
+        """
+        Returns the official string representation.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
