943fe1a3236200e30a67ccace06de54b8e86e635
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-21 14:08:07 -0700

    task3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,9 +8,16 @@ Defines a class MyList that inherits from list
 
 def is_same_class(obj, a_class):
     """
-    Returns True if obj is exactly an wise False.
+    Returns True if obj is exactly otherwise False.
     """
     return type(obj) is a_class
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if obj is an instance of.
+    """
+    return isinstance(obj, a_class)
 
 
 class MyList(list):
@@ -20,6 +27,6 @@ class MyList(list):
 
     def print_sorted(self):
         """
-        Prints the list in ascending final list.
+        Prints the list in original list.
         """
         print(sorted(self))
