97ca91c35bec38bcd181c13cac8e9d5a2b4845cd
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-21 14:22:55 -0700

    task9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This module defines the Rectangle class that.
+This module defines the Rectangle class that inherits.
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -20,9 +20,27 @@ class Rectangle(BaseGeometry):
 
         Raises:
             TypeError: If width or height is not an integer.
-            ValueError: If width or height is
+            ValueError: If width or height is to 0.
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns the string description of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        """
+        Returns the official string representation.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
