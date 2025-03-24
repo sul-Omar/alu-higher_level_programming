8c9e00090e9c9ff71499bc51fffba226dc5dffe0
sul-Omar <o.mbabonaba@alustudent.com>
2025-03-24 02:04:00 -0700

    task12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This class defines a student.
+This class defines a student.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing size n.
+
+    Args:
+        n (int): The number of rows.
+
+    Returns:
+        list: A list of lists.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
